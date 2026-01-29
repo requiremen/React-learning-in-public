5e5b3aa4dd4177f818c4f00aac7018fb8b069af5
Akshat Rastogi <154761569+requiremen@users.noreply.github.com>
2026-01-30 01:36:02 +0530

    Add App component with memoized Button component

---
+++ useCallbackjargons.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect, useMemo, memo } from 'react'
+import axios from "axios"
+import './App.css'
+
+function App(){
+  const[click,clickchange] = useState(0)
+  function inputcomponent(){
+    console.log("hi there")
+  }
+  
+  return <div>
+    <Buttoncompnent inputcomponent = {inputcomponent}/>
+      <button onClick={function(){
+        clickchange(click + 1);
+      }}>counter{click}</button>
+    </div>
+    //acha toh console pe dekha toh bhai baar baar app dono function ko render kar raha tha aisa kyun jabbki memo use kar rakha hai maine
+    // toh isliye kyunki even if memo is used react itna smart nahin hai ki woh soch sake ki dusre click pe jo input compnent aya hai woh same
+    // function se aya hai 
+    //so isliye ham call back use karte hai
+  
+}
+const Buttoncompnent = memo(function(inputcomponent){
+  const[cut,paste] = useState(0)
+console.log("rederening the button component")
+  return <div>
+   <button onClick={function(){
+      paste(cut + 1)
+    }}>clicking {cut}</button>
+
+    
+  </div>
+}
+  
+)
+
+  
+  
+export default App
