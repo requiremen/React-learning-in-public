a0b33741df26434af20685ed067ac265e97d7739
Akshat Rastogi <154761569+requiremen@users.noreply.github.com>
2026-06-27 23:41:51 +0530

    Create customhooksstater.js

---
+++ customhooksstater.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+function App(){
+  const [data,setdata]= useState([]);
+  // here the useeffect is used to mount the data and stop the required re render 
+  // until and unless i dont give any other depenedency array
+
+  useEffect(()=>{
+    axios.get("https://jsonplaceholder.typicode.com/todos").then(reponse=>{
+    setdata(reponse.data)
+  })
+  // this interval is called and its use is to check wheather is there any new
+  //data putup in the backend and accordingly render the data i mean this 
+  // thing checksup the backend and updates the state variable if there is any update 
+  //data
+   let interval = setInterval(()=>{
+     axios.get("https://jsonplaceholder.typicode.com/todos").then(reponse=>{
+    setdata(reponse.data)
+  })
+   },100000)
+   return ()=> clearInterval(interval);
+   
+  },[])
+
+  const output = data.map(d=><Todo title={d.title}/>)
+ return(
+    <div>
+      {output}
+
+    </div>
+  )
+}
+function Todo(props){
+  return(
+    <div style={{padding:20,margin:20,border:'1px solid black'}}>
+      {props.title}
+    </div>
+  )
+}
+export default App;
